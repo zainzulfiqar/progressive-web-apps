b25e6afb72bfb20ab4212d5588db1a4f81826dd8
zainzulfiqar <chzainkhan13@gmail.com>
2019-02-20 01:42:42 +0500

    my first pwa

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+var cacheName = 'pwa-6';
+var filesToCache = [
+  '/css/materialize.min.css',
+  '/js/materialize.min.js',
+  '/index.html',
+  '/',
+];
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    // return self.clients.claim();
+  });
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+    // console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
